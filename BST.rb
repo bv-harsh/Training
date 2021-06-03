9992fbd9ecd28a5de5d1ec6f21ccd3016c24c166
Harsh <harsh@blogvault.net>
2021-06-03 09:43:51 +0530

    Fix #1: Seprating Bst and Node class into different files

--- BST.rb
+++ BST.rb
@@ -1,13 +1,4 @@
-class Node
-
-	attr_accessor :left, :right, :value
-
-	def initialize(value)
-		@value = value
-		@left = nil
-		@right = nil
-	end
-end
+load "node.rb"
 
 class Bst
 
---
+++ node.rb
@@ -0,0 +1,11 @@
+class Node
+
+  attr_accessor :left, :right, :value
+
+  def initialize(value)
+    @value = value
+    @left = nil
+    @right = nil
+  end
+end
+
